d8fa5e60da1760ec5a519216f4e32c8caf833ad0
matias-yelicich-ucc <2415049@ucc.edu.ar>
2025-09-23 17:10:15 -0300

    Se agregaron imagenes de los segmentos de los vehiculos

--- pages/home_page/home.js
+++ pages/home_page/home.js
@@ -1,4 +1,5 @@
 const brandsList = document.getElementById("list-brands");
+const segmentosList = document.getElementById("list-types");
 
 // Generar cards de las marcas
 function renderMarcas(marcasObj) {
@@ -11,7 +12,7 @@ function renderMarcas(marcasObj) {
 
         card.innerHTML = `
             <a href="../catalog_page/catalog.html?marca=${encodeURIComponent(nombre)}" class="link-detalle">
-                <div class="car-image-container">
+                <div class="brand-image-container">
                     <img src="../../imagenes/marcas/${logo}.webp" alt="${nombre}">
                 </div>
                 <div class="brand-name">${nombre}</div>
@@ -24,4 +25,26 @@ function renderMarcas(marcasObj) {
 
 renderMarcas(MARCAS);
 
-// Carrusel de destacados
+// Generar cards de los segmentos
+function renderSegmentos(segmentosObj) {
+    segmentosList.innerHTML = "";
+
+    Object.values(segmentosObj).forEach(nombre => {
+        const imagen = textBase(nombre);
+        const card = document.createElement("div");
+        card.classList.add("card-type");
+
+        card.innerHTML = `
+            <a href="../catalog_page/catalog.html?segmento=${encodeURIComponent(nombre)}" class="link-detalle">
+                <div class="type-image-container">
+                    <img src="../../imagenes/segmentos/${imagen}.webp" alt="${nombre}">
+                </div>
+                <div class="type-name">${nombre}</div>
+            </a>
+        `;
+
+        segmentosList.appendChild(card);
+    });
+}
+
+renderSegmentos(TIPOS);
