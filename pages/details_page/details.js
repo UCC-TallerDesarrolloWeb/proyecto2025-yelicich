dbcfcacfafd89aa4a615783bb306ab7b4ef291b2
matias-yelicich-ucc <2415049@ucc.edu.ar>
2025-09-24 17:58:23 -0300

    Se agrego un dialog para reservar un  vehiculo

--- pages/details_page/details.js
+++ pages/details_page/details.js
@@ -122,3 +122,184 @@ function renderSimilares(auto) {
         similaresList.appendChild(card);
     });
 }
+
+// ------ MODAL -------
+// Variables
+const precioBase = auto.precio;
+let priceColor = 0
+let priceWheel = 0
+let interes = 0;
+
+// Acciones modal
+const resetModal = () => {
+    priceColor = 0;
+    priceWheel = 0;
+    interes = 0;
+
+    document.querySelectorAll(".option.active").forEach(el => el.classList.remove("active"));
+    document.getElementById("financiacion-show").style.display = "none";
+    
+    document.getElementById("colors-subtitle").classList.remove("subtitle-error");
+    document.getElementById("wheels-subtitle").classList.remove("subtitle-error");
+    document.getElementById("metod-subtitle").classList.remove("subtitle-error");
+    document.getElementById("financiation-subtitle").classList.remove("subtitle-error");
+
+    updateResumen();
+};
+
+const closeModal = () => {
+    document.getElementById("modal-pago").setAttribute("aria-hidden", "true");
+    resetModal();
+}
+
+document.querySelector(".btn-reserve").addEventListener("click", () => {
+    document.getElementById("modal-pago").setAttribute("aria-hidden", "false");
+    resetModal();
+});
+document.getElementById("close-modal").addEventListener("click", () => {
+    closeModal();
+});
+document.getElementById("modal-overlay").addEventListener("click", () => {
+    closeModal();
+});
+
+// Render inicial
+const updateResumen = () => {
+    document.getElementById("modal-title").textContent = `${auto.marca} ${auto.modelo}`;
+    document.getElementById("precio-base").textContent = `$${precioBase.toLocaleString("es-AR")}`;
+    document.getElementById("precio-color").textContent = `$${priceColor.toLocaleString("es-AR")}`;
+    document.getElementById("precio-rines").textContent = `$${priceWheel.toLocaleString("es-AR")}`;
+    document.getElementById("precio-interes").textContent = `$${interes.toLocaleString("es-AR")}`;
+    document.getElementById("precio-total").textContent = `$${(precioBase + priceColor + priceWheel + interes).toLocaleString("es-AR")}`;
+};
+updateResumen();
+
+// Colores
+document.querySelectorAll("#color-options .option").forEach(btn => {
+    btn.addEventListener("click", () => {
+        document.querySelectorAll("#color-options .option").forEach(b => b.classList.remove("active"));
+        btn.classList.add("active");
+
+        const color = btn.getAttribute("value");
+
+        switch (color) {
+            case "blanco": priceColor = 0; break;
+            case "negro": priceColor = 3000; break;
+            case "gris_oscuro": priceColor = 7000; break;
+            case "plata": priceColor = 6000; break;
+            case "azul": priceColor = 4000; break;
+            case "rojo": priceColor = 4500; break;
+            default: priceColor = 0;
+        }
+
+        document.getElementById("colors-subtitle").classList.remove("subtitle-error");
+
+        updateResumen();
+    });
+});
+
+// Rines
+document.querySelectorAll("#rines-options .option").forEach(btn => {
+    btn.addEventListener("click", () => {
+        document.querySelectorAll("#rines-options .option").forEach(b => b.classList.remove("active"));
+        btn.classList.add("active");
+
+        const wheel = btn.getAttribute("value");
+
+        switch (wheel) {
+            case "magnetite_19": priceWheel = 0; break;
+            case "perihelix_20": priceWheel = 5000; break;
+            case "velarium_21": priceWheel = 10000; break;
+            case "machina_22": priceWheel = 15000; break;
+            default: priceWheel = 0;
+        }
+        
+        document.getElementById("wheels-subtitle").classList.remove("subtitle-error");
+
+        updateResumen();
+    });
+});
+
+// Método de pago
+document.querySelectorAll("#metodo-pago .option").forEach(btn => {
+    btn.addEventListener("click", () => {
+        document.querySelectorAll("#metodo-pago .option").forEach(b => b.classList.remove("active"));
+        btn.classList.add("active");
+
+        const metodo = btn.getAttribute("value");
+        console.log(metodo);
+
+        switch (metodo) {
+            case "contado": 
+                document.getElementById("financiacion-show").style.display = "none";
+                document.querySelectorAll("#financiacion .option").forEach(b => b.classList.remove("active"));
+                interes = 0;
+            break;
+            case "tarjeta":
+                document.getElementById("financiacion-show").style.display = "block";
+            break;
+            default: 
+                document.getElementById("financiacion-show").style.display = "none";
+                document.querySelectorAll("#financiacion .option").forEach(b => b.classList.remove("active"));
+                interes = 0;
+        }
+        
+        document.getElementById("metod-subtitle").classList.remove("subtitle-error");
+        
+        updateResumen();
+    });
+});
+
+// Financiación
+document.querySelectorAll("#financiacion .option").forEach(btn => {
+    btn.addEventListener("click", () => {
+        document.querySelectorAll("#financiacion .option").forEach(b => b.classList.remove("active"));
+        btn.classList.add("active");
+
+        const cuotas = btn.getAttribute("value");
+
+        switch (cuotas) {
+            case "12cuotas": interes = precioBase * 0.05; break;
+            case "24cuotas": interes = precioBase * 0.075; break;
+            case "36cuotas": interes = precioBase * 0.10; break;
+            case "48cuotas": interes = precioBase * 0.125; break;
+            default: interes = 0;
+        }
+
+        document.getElementById("financiation-subtitle").classList.remove("subtitle-error");
+
+        updateResumen();
+    });
+});
+
+//Button
+document.getElementById("btn-confirm").addEventListener("click", () => {
+    const colorSel = document.querySelector("#color-options .active");
+    const rinSel = document.querySelector("#rines-options .active");
+    const metodoSel = document.querySelector("#metodo-pago .active");
+    const cuotasSel = document.querySelector("#financiacion .active");
+
+    if (!colorSel) {
+        alert("Debe seleccionar un color exterior.");
+        document.getElementById("colors-subtitle").classList.add("subtitle-error");
+        return;
+    }
+    if (!rinSel) {
+        alert("Debe seleccionar un tipo de rines.");
+        document.getElementById("wheels-subtitle").classList.add("subtitle-error");
+        return;
+    }
+    if (!metodoSel) {
+        alert("Debe seleccionar un método de pago.");
+        document.getElementById("metod-subtitle").classList.add("subtitle-error");
+        return;
+    }
+    if (metodoSel.getAttribute("value") === "tarjeta" && !cuotasSel) {
+        alert("Debe seleccionar una opción de financiación.");
+        document.getElementById("financiation-subtitle").classList.add("subtitle-error");
+        return;
+    }
+
+    alert("¡Gracias por su compra! Nos pondremos en contacto con usted pronto.");
+    closeModal();
+});
